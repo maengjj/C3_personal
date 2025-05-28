14b4d0b09d0e53b101485bf9392ff6d6a033bb3f
KimMinUng <minogo1054@paran.com>
2025-05-28 23:45:54 +0900

    Initial Commit

---
+++ HambJaeryoModal/HambJaeryoModalApp.swift
@@ -0,0 +1,17 @@
+//
+//  HambJaeryoModalApp.swift
+//  HambJaeryoModal
+//
+//  Created by coulson on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct HambJaeryoModalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
